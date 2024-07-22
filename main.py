9f2fcc9fcf6f6d7b36399ceac46404eb53686d0b
DanPaul12 <dan.paul.schechter@gmail.com>
2024-07-22 12:38:06 -0700

    hw16

---
+++ main.py
@@ -0,0 +1,4 @@
+import mood_response
+
+mood = input("How are you feeling today? ")
+print(mood_response.mood_response(mood))
--- mood_response.py
+++ mood_response.py
@@ -1,8 +1,7 @@
 import re
 
-def mood_response():
-    text = input("How are you feeling today?: ")
-    happy = r"good|happy|well|fine"
+def mood_response(text):
+    happy = r"good|happy|well"
     sad = r"(sad|bad|mad)"
     neutral = r"(fine|ok|comme ci comme ca)"
     var = re.search(happy, text)
@@ -11,10 +10,9 @@ def mood_response():
     if var:
         print("u rock keep it up")
     elif var2:
-        print("don worryy it gets better")
+        print("don worry it gets better")
     elif var3:
         print("get off the fence already")
     else:
         print("your mood is not valid")
 
-mood_response()
