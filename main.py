2d05a560063560fcdc995dc751b03f0e3e8d66d3
DanPaul12 <dan.paul.schechter@gmail.com>
2024-07-22 12:57:15 -0700

    hw16

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import textutils as tu
 mood = input("How are you feeling today? ")
 print(mood_response.mood_response(mood))
 
-command = input("Would you like to 1. reverse or 2. capitalize your string? Enter 1 or 2: ")
+
+command = int(input("Would you like to 1. reverse or 2. capitalize your string? Enter 1 or 2: "))
 if command == 1:
     string = input("What is your string?")
     print(tu.reverse_string(string))
